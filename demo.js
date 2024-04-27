b95ae8ee77da3a28672cede5feb353e15ba35dfc
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-04-27 22:09:48 +0700

    demo tagihan

--- demo.js
+++ demo.js
@@ -1,16 +1,16 @@
 const number = 1;
 
-const year = prompt("What year new?");
+// const year = prompt("What year new?");
 const yearNumber = 2024;
 
 // console.log(year);
-if (year == yearNumber) console.log(`Your are is great`);
+if (yearNumber == 2024) console.log(`Your are is great`);
 
-year == yearNumber
+yearNumber == "2024"
   ? console.log(`Anda Masuk Pelajaran Baru`)
   : console.log(`Anda Tinggal Kelas!!!!`);
 
-if (year == yearNumber) {
+if (2024 > yearNumber) {
   console.log(`Anda Sudah Masuk Tahun Pelajaran Baru`);
 } else {
   console.log(`Anda Tinggal Kelas`);
@@ -18,12 +18,24 @@ if (year == yearNumber) {
 
 // Harus Jeli di Type Datanya...
 
-const bulan = 4;
+let bulan = 4;
 
-bulan === "3" ? console.log(`Anda Benar`) : `Anda Salah`;
+bulan === 3
+  ? console.log(`Anda Benar`)
+  : console.log(`Anda Salah, Yang benar ${bulan}`);
 
-if (bulan === 4) {
+if (bulan <= 4) {
   console.log(`Anda benar`);
 } else {
   console.log(`Anda Salah`);
+}
+
+let tagihan = 3;
+
+if (tagihan < 4) {
+  console.log(`anda punya tgihan bulan sebelumnya`);
+} else if (tagihan > 4) {
+  console.log(`anda masuk bulan berikutnya`);
+} else if (tagihan === 4) {
+  console.log(`anda lunas ... terimakasih`);
 }
