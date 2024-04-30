15a103f6c658f13eb6838e56e7dd23814da3146b
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-01 00:01:04 +0700

    modify js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const contacts = [
   {
+    id: 1,
     name: "cahyono muslim sidiq",
     address: "Jl. Raya Sruweng No. 114",
     phone: +628567947201,
@@ -7,6 +8,7 @@ const contacts = [
     isOperator: 1
   },
   {
+    id: 2,
     name: "Tsaqif Rafasya Sidiq",
     address: "Jl. Raya Sruweng No. 115",
     phone: +628567947202,
@@ -24,7 +26,64 @@ for (let index = 0; index < contacts.length; index++) {
 }
 // contacts.isOperator ? console.log(`Administrator`) : console.log(`User`);
 
-console.log(contacts);
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
+// console.log(contacts);
+
+for (let index = 1; index < contacts.length; index++) {
+  let element = contacts[index];
+  console.log(element);
 }
+
+// LOCAL STORAGE
+
+localStorage.setItem("name", "Cahyono");
+
+let local = localStorage.getItem("name");
+
+console.log(local);
+console.log({ local });
+
+// Jika Obyek / Array Objeck maka harus di pasing JSON ke STRINGIFY => JSON.stringify dan
+// dikembalikan dengan JSON PARSE => JSON.parse
+localStorage.setItem("address", JSON.stringify(contacts));
+
+let contactList = localStorage.getItem("address", contacts);
+
+let address = JSON.parse(contactList);
+
+console.log(address, 0);
+
+// sync dan async ( teratur dari atas ke bawah dan bisa loncat)
+
+console.log("sleep");
+console.log("week up");
+console.log("sleep again");
+
+// async
+
+setTimeout(() => {
+  console.log("Hallo World");
+}, 5000);
+
+// setInterval(() => {
+//   console.log("sleep");
+//   console.log("week up");
+//   console.log("sleep again");
+// }, 5000);
+
+// promise
+
+const promiseExample = new Promise((resolve, reject) => {
+  if (1 > 0) {
+    resolve("login");
+  } else {
+    reject("not login");
+  }
+});
+
+console.log(promiseExample);
+
+// then chath
+
+promiseExample
+  .then((result) => console.log(result))
+  .catch((reject) => console.log(reject));
