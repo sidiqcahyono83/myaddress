e479a2e3f16db103f96cd83b15f3d87bd0cb3778
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-05-07 22:34:43 +0700

    update local sotareg

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const contactsListElement = document.getElementById("list-contacts");
 const contacts = [
   {
@@ -8,26 +7,13 @@ const contacts = [
     phone: +628567947201,
     email: "cdq.cahyono@gmail.com",
     level: 1
-=======
-const contactsListElement = document.getElementById("contacts");
-
-const contacts = [
-  {
-    id: 1,
-    name      : "cahyono muslim sidiq",
-    address   : "Jl. Raya Sruweng No. 114",
-    phone     : "+628567947201",
-    email     : "cdq.cahyono@gmail.com",
-    isOperator: 1,
->>>>>>> d66da67ee39dbe1496dd2c86e0ad7bd98d9c4402
   },
   {
     id: 2,
     name: "Tsaqif Rafasya Sidiq",
     address: "Jl. Raya Sruweng No. 115",
-    phone: "+628567947202",
+    phone: +628567947202,
     email: "tasqif.sidiq@gmail.com",
-<<<<<<< HEAD
     level: 0
   }
 ];
@@ -35,7 +21,6 @@ const contacts = [
 function listContacts() {
   let storedContacts = localStorage.getItem("contacts");
   if (storedContacts) {
-    // Jika terdapat data yang tersimpan di local storage
     storedContacts = JSON.parse(storedContacts);
     const contasString = storedContacts.map((contact) => {
       const level = contact.level == 1 ? `Administrator` : `User`;
@@ -48,9 +33,8 @@ function listContacts() {
       <p>Leve  : ${level}</p>
       </li>`;
     });
-    contactsListElement.innerHTML = contasString.join(""); // Menggunakan join("") untuk menggabungkan array menjadi string
+    contactsListElement.innerHTML = contasString.join("");
   } else {
-    // Jika tidak ada data yang tersimpan di local storage, menggunakan data dari variabel
     const contasString = contacts.map((contact) => {
       const level = contact.level == 1 ? `Administrator` : `User`;
       return `<li>
@@ -62,33 +46,8 @@ function listContacts() {
       <p>Leve  : ${level}</p>
       </li>`;
     });
-    contactsListElement.innerHTML = contasString.join(""); // Menggunakan join("") untuk menggabungkan array menjadi string
+    contactsListElement.innerHTML = contasString.join("");
   }
 }
 
-// Memanggil fungsi listContacts saat halaman dimuat
 listContacts();
-=======
-    isOperator: 0,
-  }
-];
-
-
-function showContacts() {
-  const showContactsString = contacts.map((contact) => {
-      const level = contact.isOperator == 1 ? `Administrator` : ` User`;
-
-      return `<li>
-              <h2>No      : ${contact.id}</h2>
-              <p>Name     : ${contact.name}</p>
-              <p>Address  : ${contact.address}</p>
-              <p>Email    : ${contact.email}</p>
-              <p>Phone    : ${contact.phone}</p>
-              <p>Level    : ${level}</p>
-              </li>
-              `
-    });
-  contactsListElement.innerHTML = showContactsString;
-}
-showContacts();
->>>>>>> d66da67ee39dbe1496dd2c86e0ad7bd98d9c4402
